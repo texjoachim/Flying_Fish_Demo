747a8962c07de4f2919e5c75d1613d92d3596730
Joachim Ziebs <joachim@ziebs.net>
2018-01-12 22:08:57 +0100

    Added rain sensor, moved library to own project.

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,27 @@
 
 // the moisture sensor
 flyingFish moistureSensor(3, A1);
+flyingFish rainSensor(4, A2);
 
 void setup() {
     Serial.begin(9600);
-    Serial.println("Flying Fish Helper Demo. Using a Moisture Sensor.");
+    Serial.println("Flying Fish Helper Demo. Using a Moisture Sensor and a rain sensor");
 }
 
 void loop() {
+    Serial.println("MOISTURE SENSOR");
     Serial.print("Digital reading: ");
     Serial.println(moistureSensor.digitalread());
     delay(1000);
     Serial.println();
     Serial.print("Analog reading: ");
     Serial.println(moistureSensor.analogread());
+    Serial.println("RAIN SENSOR");
+    Serial.print("Digital reading: ");
+    Serial.println(rainSensor.digitalread());
+    delay(1000);
+    Serial.println();
+    Serial.print("Analog reading: ");
+    Serial.println(rainSensor.analogread());
     delay(2000);
 }
